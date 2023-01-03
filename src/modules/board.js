07a7ba1e77249e4e0166407ebb044970964b788f
Luis Zubia <soyzubia@gmail.com>
2023-01-03 16:56:42 -0600

    Add in renderBoard method two divs one for the list of scores, and another one for the add score form.

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,12 +1,15 @@
+import Api from './api.js';
+
 class Board {
   constructor() {
     this.title = 'Leaderboard';
+    this.api = new Api();
   }
 
   renderBoard = () => {
     const board = document.getElementById('leaderboard');
 
-    board.innerHTML = `<h1>${this.title}</h1>`;
+    board.innerHTML = `<h1>${this.title}</h1><div id="scores"></div><div id="addscore"></div>`;
   }
 }
 
