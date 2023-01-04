b3da57f02194afbfc06bacc0e37290386fc43cfc
Luis Zubia <soyzubia@gmail.com>
2023-01-03 18:03:22 -0600

    Import scores module into the board, to have a recent scores section.

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,15 +1,18 @@
 import Api from './api.js';
+import Scores from './scores.js';
 
 class Board {
   constructor() {
+    this.div = document.getElementById('leaderboard');
     this.title = 'Leaderboard';
     this.api = new Api();
+    this.Scores = new Scores();
   }
 
   renderBoard = () => {
-    const board = document.getElementById('leaderboard');
-
-    board.innerHTML = `<h1>${this.title}</h1><div id="scores"></div><div id="addscore"></div>`;
+    this.div.innerHTML = '';
+    this.div.innerHTML = `<h1>${this.title}</h1><div id="scores"></div><div id="addscore"></div>`;
+    this.Scores.renderScores(this.api.getScores());
   }
 }
 
