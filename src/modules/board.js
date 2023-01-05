143a30df199da580299e3873b3b76838e39566d0
Luis Zubia <soyzubia@gmail.com>
2023-01-04 19:57:51 -0600

    the api now gets the name of the game to create the board, method renderBoard now is async for calling await to api.retrieveScores.

--- src/modules/board.js
+++ src/modules/board.js
@@ -5,18 +5,19 @@ import Addscore from './addscore.js';
 class Board {
   constructor() {
     this.div = document.getElementById('leaderboard');
-    this.api = new Api();
+    this.api = new Api('leaderBoardGameInfinity');
     this.Scores = new Scores();
     this.AddScore = new Addscore();
   }
 
-  renderBoard = () => {
+  renderBoard = async () => {
     this.div.innerHTML = `<div>
                             <section id="scores"></section>
                             <section id="addscore"></section>
                           </div>`;
-    this.Scores.renderScores(this.api.getScores());
+    this.Scores.renderScores();
     this.AddScore.renderAddScore();
+    this.Scores.renderScoreList(await this.api.retrieveScores());
   }
 }
 
